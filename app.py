fcb3c2bac31c606c86375cd00d0e31c9db1d6510
This-swapnil <sswapnil0098@gmail.com>
2021-10-19 21:36:53 +0530

    last commit

--- app.py
+++ app.py
@@ -20,25 +20,15 @@ def predict():
     if request.method == "POST":
         try:
             data_req = dict(request.form)
-            print(data_req)
             data = data_req.values()
             data = [list(map(float,data))]
-            print(data)
             scaled = saved_scaler.transform(data)
-            print(scaled)
             response = model.predict(scaled)
-            print(response)
             return render_template('result.html', result=response[0])
         except Exception as e:
             error = {'error': e}
-            print(error)
             return render_template('404.html', error=error)
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-# [-0.41978194,  0.28482986, -1.2879095 , -0.27259857, -0.14421743,
-#         0.41367189, -0.12001342,  0.1402136 , -0.98284286, -0.66660821,
-#        -1.45900038,  0.44105193, -1.0755623 ]
